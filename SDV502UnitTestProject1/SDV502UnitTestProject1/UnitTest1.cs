85f489ee9b7eca362904a226d3d08f01423fdb77
hailejared <jared.o@live.com>
2017-08-14 13:11:17 +1200

    Test commit.

---
+++ SDV502UnitTestProject1/SDV502UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FastwayCourier;
+
+namespace SDV502UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            string[] pinkZone = new string[11] { "Nelson", "Motueka", "Mapua", "Atawhai", "Matai", "Hope", "Brightwater", "Wakefield", "Renwick", "Picton", "Blenheim" };
+
+            //arrange
+            ParcelQuoteFromNelson quoteNelson = new ParcelQuoteFromNelson();
+            //act
+            string ticketColour = quoteNelson.GetDestinationZone("Nelson"); //town name 
+            //assert
+            Assert.AreEqual("Pink", ticketColour); //ticket colour
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //arrange
+            ParcelQuoteFromNelson quoteBlenheim = new ParcelQuoteFromNelson();
+            //act
+            string ticketColour = quoteBlenheim.GetDestinationZone("Blenheim"); //town name 
+            //assert
+            Assert.AreEqual("Pink", ticketColour); //ticket colour
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            ParcelQuoteFromNelson quoteFromNelson = new ParcelQuoteFromNelson();
+            string[] pinkZones = new string[] { "Nelson", "Motueka", "Mapua", "Atawhai", "Matai", "Hope", "Brightwater", "Wakefield", "Renwick", "Picton", "Blenheim" };
+
+            foreach (string towns in pinkZones)
+            {
+                string ticketColour = quoteFromNelson.GetDestinationZone(towns);
+                Assert.AreEqual("Pink", ticketColour);
+            }
+        }
+
+
+    }
+}
